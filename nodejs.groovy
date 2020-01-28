0b1f50fb61e20c4dab0422cfbbacbe9b7b93da1a
uni51 <uni51@users.noreply.github.com>
2020-01-28 15:45:12 +0900

    Add DSL

---
+++ nodejs.groovy
@@ -0,0 +1,18 @@
+job('NodeJS Job') {
+    scm {
+        git('https://github.com/uni51/devops_sample_nodejs.git') {  node ->
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@example.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs_v11')
+    }
+    steps {
+        shell("npm install")
+        shell("npm test")
+    }
+}
